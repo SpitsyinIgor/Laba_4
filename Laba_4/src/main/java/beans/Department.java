d579d57db409bcacc081b2f8d30c2ef2b1ad61e1
SpitsyinIgor <1overflash1@gmail.com>
2024-12-12 20:32:42 +0300

    Update Department.java

--- Laba_4/src/main/java/beans/Department.java
+++ Laba_4/src/main/java/beans/Department.java
@@ -1,1 +1,31 @@
 package beans;
+
+public class Department {
+    private int ID;
+    private String name;
+
+    Department(int ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public String toString() {
+        return String.valueOf(ID) + ' ' + name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
